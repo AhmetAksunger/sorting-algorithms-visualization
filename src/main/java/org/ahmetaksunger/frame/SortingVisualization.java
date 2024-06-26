be6b17061f8a3a6c7594428d9b1ccbcf231381e4
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2024-06-26 21:04:43 +0300

    Create SortingVisualization class

---
+++ src/main/java/org/ahmetaksunger/frame/SortingVisualization.java
@@ -0,0 +1,91 @@
+package org.ahmetaksunger.frame;
+
+
+import org.ahmetaksunger.exception.UnequalBoxWidthsException;
+import org.ahmetaksunger.model.Box;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+/**
+ * This is a base class for different sorting visualizations. It extends {@link JFrame} and
+ * provides a graphical visualization of a list of boxes.
+ */
+public class SortingVisualization extends JFrame {
+
+    private final List<Box> boxes;
+    private final int boxSpacing;
+
+    public SortingVisualization(List<Box> boxes, int boxSpacing) {
+        this.boxes = boxes;
+        this.boxSpacing = boxSpacing;
+        validateBoxWidths();
+        setFrame();
+    }
+
+    /**
+     * Overrides the paint method to draw each box in the list using its specified color,
+     * position, width, and height.
+     *
+     * @param g The Graphics context used for painting.
+     */
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        for (Box box : boxes) {
+            g.setColor(box.getColor());
+            g.fillRect(box.getX(), box.getY(), box.getWidth(), box.getHeight());
+        }
+    }
+
+    /**
+     * Animates the sorting done state by changing each box's color to WHITE and then
+     * to GRAY, with a short delay between changes.
+     *
+     * @throws InterruptedException If the thread is interrupted while sleeping.
+     */
+    public void sortingDoneAnimation() throws InterruptedException {
+        for (Box box : boxes) {
+            box.setColor(Color.WHITE);
+            super.repaint();
+            Thread.sleep(50);
+            box.setColor(Color.GRAY);
+            super.repaint();
+        }
+    }
+
+    /**
+     * Getter for the property List of boxes
+     *
+     * @return List of boxes
+     */
+    public List<Box> getBoxes() {
+        return boxes;
+    }
+
+    /**
+     * Private method to set up the frame.
+     */
+    private void setFrame() {
+        setTitle("Black Frame");
+
+        int n = boxes.size();
+        int width = (n * boxes.get(0).getWidth()) + ((n - 1) * (boxSpacing - boxes.get(0).getWidth()));
+
+        setSize(width, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.BLACK);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void validateBoxWidths() {
+
+        int boxWidth = boxes.get(0).getWidth();
+
+        if (!boxes.stream().allMatch(box -> box.getWidth() == boxWidth))
+            throw new UnequalBoxWidthsException("Not all boxes have the same width");
+    }
+}
+
